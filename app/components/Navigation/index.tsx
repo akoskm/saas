a328ce7edf6825038e6df0dfefdf84e38b497f95
Akos Komuves <akoskm@gmail.com>
2023-11-30 14:33:17 +0100

    nav logo should go to /

--- app/components/Navigation/index.tsx
+++ app/components/Navigation/index.tsx
@@ -99,11 +99,13 @@ export default function Navigation({ loginId }: { loginId?: string | null }) {
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
-                  <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
-                  />
+                  <Link to="/">
+                    <img
+                      className="h-8 w-auto"
+                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                      alt="Your Company"
+                    />
+                  </Link>
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
